9033aadec76e7db460e064d796e797d9e652b53e
RobertasOlsanovas <107546775+RobertasOlsanovas@users.noreply.github.com>
2022-08-29 17:25:16 +0300

    gif compresion and lazy loading for images

--- dist/js/app.js
+++ dist/js/app.js
@@ -786,6 +786,35 @@
         document.documentElement.classList.add(className);
       });
     }
+    const images = document.querySelectorAll('[src]');
+
+    function preloadImage(img) {
+      const src = img.getAttribute('src');
+      if (!src) {
+        return;
+      }
+      img.src = src;
+    }
+
+    const imgOptions = {
+      threshold: 0,
+      rootMargin: '0px 0px 0px 0px',
+    };
+
+    const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          return;
+        } else {
+          preloadImage(entry.target);
+          imgObserver.unobserve(entry.target);
+        }
+      });
+    }, imgOptions);
+    images.forEach((image) => {
+      imgObserver.observe(image);
+    });
+
     let isMobile = {
       Android: function () {
         return navigator.userAgent.match(/Android/i);
